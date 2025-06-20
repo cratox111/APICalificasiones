be46e31eee5cd38df2baa48a22cda3d6a0eb21bb
cratox111 <diocgon2011@gmail.com>
2025-06-19 21:40:49 -0600

    Agregue la posibilidad que los estudiantes se puedan añadir a clases

--- app/controllers/Students.py
+++ app/controllers/Students.py
@@ -1,7 +1,9 @@
-from flask import jsonify, request
+from flask import jsonify, request, json
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from app.models.Students import Students
+from app.models.Class import Classes
 
 def newUser():
     inputData = request.get_json()
@@ -40,6 +42,38 @@ def loginUser():
 
     if not student or not check_password_hash(student.password, password):
         return jsonify({'message': 'Incorrect email or password'}), 401
+    
+    access_token = create_access_token(identity=student.id)
 
     return jsonify({'message': 'Login successful'}), 200
+
+
+@jwt_required()
+def joinClass():
+    inputData = request.get_json()
+
+    name_class = inputData.get('name')
+    lounge = inputData.get('lounge')
+    hour = inputData.get('hour')
+    teacher = inputData.get('teacher')
+
+    if not name_class or not lounge or not hour or not teacher:
+        return jsonify({'message':'not data'})
     
+    clase = Classes.query.filter_by(name_class=name_class, lounge=lounge, hour=hour, teacher=teacher)
+    if not clase:
+        return jsonify({'message':'not exits class'})
+    
+    current_user = get_jwt_identity()
+    student = Students.query.get(current_user)
+    
+    if not student:
+        return jsonify({'message':'Not exits student'})
+    
+    if clase in student.classes:
+        return jsonify({'message':'Class exits'})
+    
+    student.classes.append(clase)
+    db.session.commit()
+
+    return jsonify({'message':'Add class successful'})
